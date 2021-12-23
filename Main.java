71a035fc11033245d22b46866f29c1391505dc4c
Jose Wuiler Pozzi <wuiler@gmail.com>
2021-12-23 13:56:28 +0000

    adding colours

--- Main.java
+++ Main.java
@@ -1,10 +1,28 @@
+
 /*
-*
+* A Christmas Tree Draw!
+
+ * @author      José Wuiler Pozzi <wuiler@gmail.com>
+ * @version     1.0
 */
 class Main {
+
+	//the star like me ;-)
+	private static String shineStar = "*";
+	// the leafs of the tree
+	private static String leafs = "1";
+	// the root od the tree
+	private static String root = "0";
+	// grass
+	private static String grass = "/\\";
+
   public static void main(String[] args) {
 		//gretting messages !!! to the PODs 
-    System.out.println("\nFelices Fiestas\n¡POD ACONCAGUA!\n");
+		Print.coloursOut(ConsoleColors.RED_BACKGROUND,"Felices Fiestas",true);
+		Print.coloursOut(ConsoleColors.GREEN_BACKGROUND,"¡POD ACONCAGUA!",true);
+		// give some space
+		System.out.println(" ");
+
 		int c = 0;
 		/*
 		* First of all, we are going to draw the shine Star *
@@ -17,7 +35,7 @@ class Main {
 				System.out.print(" ");
 			}
 			// then we print out the Star
-			System.out.print("*");
+			Print.coloursLine(ConsoleColors.YELLOW_BACKGROUND_BRIGHT, shineStar, true);
 
 		// give a break ;-)
 		System.out.println();			
@@ -34,7 +52,7 @@ class Main {
     		System.out.print(" ");
    		for (c = 0; c < (2 * i + 1); c++)
 				// each leaf, in binary one. :-)
-    		System.out.print("1");
+    		Print.coloursLine(ConsoleColors.GREEN_BRIGHT, leafs, true);
    		System.out.println("");
   	}
 		/*
@@ -45,7 +63,7 @@ class Main {
   		for (int j = 0; j < (c/2); j++) 
     		System.out.print(" ");
 			 for (int m = 0; m < 3; m++)
-    		System.out.print("0");
+			 	Print.coloursLine(ConsoleColors.BLUE_BOLD, root, true);    		
    		System.out.println("");
   	}
 		
@@ -53,15 +71,13 @@ class Main {
 		* Like a bonus track, adding some grass to make it beautiful
 		*/
 		for (int n=0; n<=(c/2); n++) 
-			System.out.print("/\\");
-
-
+			Print.coloursLine(ConsoleColors.GREEN, grass, true);
+		// give a jump break!
 		System.out.println("");
 		/*
 		* That's All Folks!!!
 		*/
 
 	}
-
 
 }
